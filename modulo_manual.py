69ef133f114eb64a612d64ef8936c4856f32111c
Lirio Valentin Reategui Rengifo <lirio9192@outlook.com>
2025-10-25 12:36:45 -0300

    Atualizar o modulo_manual.py

--- modulo_manual.py
+++ modulo_manual.py
@@ -1,11 +1,8 @@
-def modulo(numero):
-    resto = numero / 2
-    absoluto = int(resto) 
-    if resto - absoluto == 0.5:
-        modulo =  resto - (resto - 1)
+def modulo(x):
+    quociente = x / 2
+    valor_inteiro = int(quociente) 
+    if quociente - valor_inteiro == 0.5:
+        resto =  quociente - (quociente - 1)
     else:
-        modulo = resto - resto
-    return int(modulo)
-numero = int(input('Digite um número: '))
-resto = modulo(numero)
-print(f"Resto é igual a {resto}")
+        resto = quociente - quociente
+    return int(resto)
